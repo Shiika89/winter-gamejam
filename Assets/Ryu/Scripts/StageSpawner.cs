7cc1d90492048220f3567b766772be274c3da9ae
RyuKento <83810925+RyuKento@users.noreply.github.com>
2021-12-21 16:07:56 +0900

    背景のスクロール

---
+++ Assets/Ryu/Scripts/StageSpawner.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StageSpawner : MonoBehaviour
+{
+    public List<GameObject> StagePattern;
+    private Vector2 m_startPos;
+    private float m_screenHalfWidth;
+    public GameObject screenObject;
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_startPos = transform.position;
+        m_screenHalfWidth = transform.localScale.x/2;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (screenObject.transform.position.x < 0)
+        {
+            GenerateScreen();
+        }
+    }
+
+    public void GenerateScreen()
+    {
+
+    }
+}
