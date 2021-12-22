7dc2a5252cb40c83e20b33becdc4b379ffc53c59
TsuguhikoOkada <tsugu.pc.academy@gmail.com>
2021-12-22 14:07:17 +0900

    金色の羽アイテムとパーティクルのプレハブを追加

--- Assets/Tsuguhiko/Tsugu_Scripts/TimeManager.cs
+++ Assets/Tsuguhiko/Tsugu_Scripts/TimeManager.cs
@@ -18,7 +18,7 @@ public class TimeManager : MonoBehaviour
     /// <summary>
     ///  終了時間
     /// </summary>
-    float _finishTime;
+    float _countTime;
 
     /// <summary>
     ///  Textコンポーネントと残り時間
@@ -28,25 +28,31 @@ public class TimeManager : MonoBehaviour
         // Textコンポーネント取得
         _timeText = GetComponent<Text>();
         // 残り時間を設定
-        _finishTime = _gameTime;
+        _countTime = _gameTime;
     }
 
     
     void Update()
     {
         // 残り時間を計算する
-        _finishTime += Time.deltaTime;
+        _countTime += Time.deltaTime;
 
-        // 1441秒(24時)になったらテキストに表示されている秒数を強制的に0秒に表示させる。その後、Scene遷移の予定
-        if (_finishTime > 1441.0f)
+        // 1441秒(24時)超えたらテキストに表示されている秒数を強制的に0秒に表示させる。その後、Scene遷移の予定
+        if (_countTime > 1441.0f)
         {
-            _finishTime = 0.0f;
+            _countTime = 0.0f;
         }
+        // 361秒(6時)超えたらイベント発生 (現時点でイベント内容と時間は未定)
+        else if (_countTime > 361.0f)
+        {
+
+        }
+        
 
         // 残り時間[s]を60で割った整数値
-        int minutes = Mathf.FloorToInt(_finishTime / 60F);
+        int minutes = Mathf.FloorToInt(_countTime / 60F);
         // 残り時間[s]からminutesで求めた整数値から60をかけた差
-        int seconds = Mathf.FloorToInt(_finishTime - minutes * 60);
+        int seconds = Mathf.FloorToInt(_countTime - minutes * 60);
         // minutesとsecondsの結果をストリング変換してテキスト表示
         _timeText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
     }
