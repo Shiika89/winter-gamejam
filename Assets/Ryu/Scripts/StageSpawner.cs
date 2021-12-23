f2beb8e3f9f9e9ac4d43a4ffa41030645d3982ed
RyuKento <83810925+RyuKento@users.noreply.github.com>
2021-12-23 12:05:33 +0900

    時間感知の追加(分)

--- Assets/Ryu/Scripts/StageSpawner.cs
+++ Assets/Ryu/Scripts/StageSpawner.cs
@@ -30,8 +30,8 @@ public class StageSpawner : MonoBehaviour
 
     private int randomNum;
 
-    [SerializeField] int m_changeDifficultyTime;
-
+    [SerializeField] int m_changeDifficultyHour;
+    [SerializeField] int m_changeDifficultyMinute;
     [SerializeField] int m_difficultyNum;
 
     //private float m_timeCount;
@@ -70,7 +70,7 @@ public class StageSpawner : MonoBehaviour
             {
                 StageGenelate(m_posX);
             }
-            if (ManagerTime.CurrentHour >= m_changeDifficultyTime)
+            if (ManagerTime.CurrentHour >= m_changeDifficultyHour && ManagerTime.CurrentMinute >= m_changeDifficultyMinute)
             {
                 isHardMode = true;
             }
